4464c392cd06e51001b5bd6b18e14a806d8ed98d
jihoho <xkftn94@naver.com>
2021-04-09 07:42:47 +0900

    Feat : 회원 도메인 개발

---
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -0,0 +1,40 @@
+package jpabook.jpashop.repository;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by IntelliJ IDEA
+ * User: hojun
+ * Date: 2021-04-09 Time: 오전 6:12
+ */
+@Repository
+@RequiredArgsConstructor
+public class MemberRepository {
+
+    private final EntityManager em;
+
+    public void save(Member member) {
+        em.persist(member);
+    }
+
+    public Member findOne(Long id) {
+        return em.find(Member.class, id);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where m.name= :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
+}
